7f37dce5ed3a83ac524ac5234f30bfdff25fce71
Piyush-Kumar-Mishra <piyushmishra.0220@gmail.com>
2026-03-04 22:04:02 +0530

    Fixed EditProjectScreen.kt to use the new AuthorizedAsyncImage.

--- app/src/main/java/com/example/linkit/view/screens/EnterDetailsScreen.kt
+++ app/src/main/java/com/example/linkit/view/screens/EnterDetailsScreen.kt
@@ -16,11 +16,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Person
